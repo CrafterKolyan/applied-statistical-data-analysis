245c7b21c0f62f8aa68080b6c928f3cd43f3e8aa
Nikolay Korolev <korolevns98@gmail.com>
2020-02-18 00:52:56 +0300

    Add automatic execution of all notebooks

--- util/run_all_notebooks.py
+++ util/run_all_notebooks.py
@@ -1,7 +1,7 @@
-#/usr/bin/python3
 import os
 import re
 import sys
+import copy
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from itertools import repeat
 
@@ -26,14 +26,17 @@ def run_notebook(path, preprocessor):
   with open(path, 'r', encoding='utf-8') as notebook_file:
     notebook = nbformat.read(notebook_file, as_version=nbformat.NO_CONVERT)
 
+  initial_notebook = copy.deepcopy(notebook)
+
   try:
     preprocessor.preprocess(notebook, {'metadata': {'path': os.path.dirname(path)}})
   except CellExecutionError as e:
     result = False
-    print(" FAIL!")
+    print(" FAIL!", "CHANGED!" if notebook != initial_notebook else "")
     print(e)
   else:
-    print(" OK!")
+    print(" OK!", "CHANGED!" if notebook != initial_notebook else "")
+  sys.stdout.flush()
 
   with open(path, 'w', encoding='utf-8') as notebook_file:
     nbformat.write(notebook, notebook_file)
